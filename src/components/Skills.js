f9bf8c68b8b7f654ddcf9015d750dd7b859748cd
wnal4634 <wnal4634@naver.com>
2023-08-16 23:41:13 +0900

    스킬 수정(이미지로)

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,20 @@
 import React from "react";
 import styles from "css/Skills.module.css";
 import images from "components/ImagesImport";
+import {
+    SiJavascript,
+    SiBootstrap,
+    SiHtml5,
+    SiCss3,
+    SiReact,
+    SiGit,
+    SiGithub,
+    SiFigma,
+    SiMysql,
+    SiDart,
+    SiFlutter,
+} from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa";
 
 const Skills = () => {
     return (
@@ -8,22 +22,66 @@ const Skills = () => {
             <h2>🔧 Skills</h2>
             <hr />
             <div className={styles.skills_img}>
-                <div>
-                    <img src={images.html} />
-                    <img src={images.css} />
-                    <img src={images.js} />
-                    <img src={images.reactJs} />
-                    <img src={images.dart} />
-                    <img src={images.flutter} />
-                    <img src={images.git} />
+                <div className={styles.skills_img_row}>
+                    <div>
+                        <img src={images.html} alt="html 이미지" />
+                        <span>HTML</span>
+                    </div>
+                    <div>
+                        <img src={images.css} alt="css 이미지" />
+                    </div>
+                    <div>
+                        <img src={images.js} alt="js 이미지" />
+                    </div>
+                    <div>
+                        <img src={images.reactJs} alt="reactJs 이미지" />
+                    </div>
+                    <div>
+                        <img src={images.dart} alt="dart 이미지" />
+                    </div>
+                    <div>
+                        <img src={images.flutter} alt="flutter 이미지" />
+                    </div>
+                </div>
+                <div className={styles.skills_img_row}>
+                    <div>
+                        <img src={images.github} alt="github 이미지" />
+                    </div>
+                    <div>
+                        <img src={images.git} alt="git 이미지" />
+                    </div>
+                    <div>
+                        <img src={images.bootstrap} alt="bootstrap 이미지" />
+                    </div>
+                    <div>
+                        <img src={images.figma} alt="figma 이미지" />
+                    </div>
+                    <div>
+                        <img src={images.nodeJs} alt="nodeJs 이미지" />
+                    </div>
+                    <div>
+                        <img src={images.mysql} alt="mysql 이미지" />
+                    </div>
+                </div>
+                {/* <div>
+                    <div>
+                        <SiHtml5 size={50} />
+                        <span className={styles.tooltip}>HTML</span>
+                    </div>
+                    <SiCss3 size={50} />
+                    <SiJavascript size={50} />
+                    <SiReact size={50} />
+                    <SiDart size={50} />
+                    <SiFlutter size={50} />
+                    <SiGit size={50} />
                 </div>
                 <div>
-                    <img src={images.github} />
-                    <img src={images.bootstrap} />
-                    <img src={images.figma} />
-                    <img src={images.nodeJs} />
-                    <img src={images.mysql} />
-                </div>
+                    <SiGithub size={50} />
+                    <SiBootstrap size={50} />
+                    <SiFigma size={50} />
+                    <FaNodeJs size={50} />
+                    <SiMysql size={50} />
+                </div> */}
             </div>
         </div>
     );
