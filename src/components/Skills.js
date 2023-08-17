702b2ca9b4a58f4b28928c252559054d7dfe51ad
wnal4634 <wnal4634@naver.com>
2023-08-17 21:34:15 +0900

    스킬 hover

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -29,38 +29,49 @@ const Skills = () => {
                     </div>
                     <div>
                         <img src={images.css} alt="css 이미지" />
+                        <span>CSS</span>
                     </div>
                     <div>
                         <img src={images.js} alt="js 이미지" />
+                        <span>JS</span>
                     </div>
                     <div>
                         <img src={images.reactJs} alt="reactJs 이미지" />
+                        <span>React</span>
                     </div>
                     <div>
                         <img src={images.dart} alt="dart 이미지" />
+                        <span>Dart</span>
                     </div>
                     <div>
                         <img src={images.flutter} alt="flutter 이미지" />
+                        <span>Flutter</span>
                     </div>
                 </div>
                 <div className={styles.skills_img_row}>
                     <div>
                         <img src={images.github} alt="github 이미지" />
+                        <span>Github</span>
                     </div>
                     <div>
                         <img src={images.git} alt="git 이미지" />
+                        <span>Git</span>
                     </div>
                     <div>
                         <img src={images.bootstrap} alt="bootstrap 이미지" />
+                        <span>Bootstrap</span>
                     </div>
                     <div>
                         <img src={images.figma} alt="figma 이미지" />
+                        <span>Figma</span>
                     </div>
                     <div>
                         <img src={images.nodeJs} alt="nodeJs 이미지" />
+                        <span>NodeJS</span>
                     </div>
                     <div>
                         <img src={images.mysql} alt="mysql 이미지" />
+                        <span>MySQL</span>
                     </div>
                 </div>
                 {/* <div>
