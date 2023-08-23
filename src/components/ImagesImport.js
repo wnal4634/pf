335f0dd8447aee750e4300641a1c356aeecc03e9
wnal4634 <wnal4634@naver.com>
2023-08-23 23:23:30 +0900

    작업물 수정, 추가

--- src/components/ImagesImport.js
+++ src/components/ImagesImport.js
@@ -15,6 +15,20 @@ import js_1_img14 from "img/js_1/14.png";
 import js_1_img15 from "img/js_1/15.png";
 import js_1_img16 from "img/js_1/16.png";
 import js_1_img17 from "img/js_1/17.png";
+import js_1_2_img1 from "img/js_1_2/1.png";
+import js_1_2_img2 from "img/js_1_2/2.png";
+import js_1_2_img3 from "img/js_1_2/3.png";
+import js_1_2_img4 from "img/js_1_2/4.png";
+import js_1_2_img5 from "img/js_1_2/5.png";
+import js_1_2_img6 from "img/js_1_2/6.png";
+import js_1_2_img7 from "img/js_1_2/7.png";
+import js_1_2_img8 from "img/js_1_2/8.png";
+import js_1_2_img9 from "img/js_1_2/9.png";
+import js_1_2_img10 from "img/js_1_2/10.png";
+import js_1_2_img11 from "img/js_1_2/11.png";
+import js_1_2_img12 from "img/js_1_2/12.png";
+import js_1_2_img13 from "img/js_1_2/13.png";
+import js_1_2_img14 from "img/js_1_2/14.png";
 import js_2_img1 from "img/js_2/1.png";
 import js_2_img2 from "img/js_2/2.png";
 import js_2_img3 from "img/js_2/3.png";
@@ -55,6 +69,20 @@ const images = {
     js_1_img15,
     js_1_img16,
     js_1_img17,
+    js_1_2_img1,
+    js_1_2_img2,
+    js_1_2_img3,
+    js_1_2_img4,
+    js_1_2_img5,
+    js_1_2_img6,
+    js_1_2_img7,
+    js_1_2_img8,
+    js_1_2_img9,
+    js_1_2_img10,
+    js_1_2_img11,
+    js_1_2_img12,
+    js_1_2_img13,
+    js_1_2_img14,
     js_2_img1,
     js_2_img2,
     js_2_img3,
--- src/components/Project.js
+++ src/components/Project.js
@@ -156,28 +156,180 @@ const JsPractice = () => {
                         </SwiperSlide>
                     </Swiper>
                     <div className={styles.project_content}>
-                        레시피 공유 및 밀키트 판매 앱입니다.
-                        <br />
-                        자바와 PHP, MySQL을 활용해 제작했고, 앱 내 결제는
-                        부트페이를 연동했습니다. (테스트 결제로 실제 결제가
-                        이뤄지지 않습니다.)
-                        <br />
-                        개발인원: 4명
-                        <br />
-                        사용자 페이지 주요 기능
-                        <br />
-                        회원가입, 로그인, 회원정보 수정, 글 작성 및 수정, 삭제,
-                        밀키트로 제작할 레시피 투표, 밀키트 구매
-                        <br />
-                        관리자 페이지 주요 기능
-                        <br />
-                        로그인, 회원관리, 게시글 관리, 공지사항 작성 및 관리,
-                        판매 관련 통계 데이터 시각화, 주문목록 확인
+                        <div className={styles.content}>
+                            <div>
+                                레시피 공유 및 밀키트 판매 앱입니다.
+                                <br />
+                                앱은 자바와 MySQL을 활용해 제작했고, 앱 내
+                                결제는 부트페이를 연동했습니다. (테스트 결제로
+                                실제 결제가 이뤄지지 않습니다.) 관리자 페이지가
+                                있어 회원 정보 등을 확인할 수 있습니다. 관리자
+                                페이지(웹)는 PHP와 부트스트랩을 이용했습니다.
+                            </div>
+                            <div>개발인원: 4명</div>
+                            <div>
+                                사용자 페이지 주요 기능
+                                <br />
+                                회원가입, 로그인, 회원정보 수정, 글 작성 및
+                                수정, 삭제, 좋아요, 밀키트로 제작할 레시피 투표,
+                                밀키트 구매
+                                <br />
+                                관리자 페이지 주요 기능
+                                <br />
+                                로그인, 회원관리, 게시글 관리, 공지사항 작성 및
+                                관리, 판매 관련 통계 데이터 시각화, 주문목록
+                                확인
+                            </div>
+                        </div>
                         <div className={styles.skill_shortcut}>
                             <div className={styles.skills}>
                                 <div style={{ color: colors.Java }}>Java</div>
                                 <div style={{ color: colors.PHP }}>PHP</div>
                                 <div style={{ color: colors.MySQL }}>MySQL</div>
+                            </div>
+                            <div className={styles.shortcut}>
+                                <div
+                                    onClick={() => {
+                                        window.open(recipe_github);
+                                    }}
+                                >
+                                    깃허브
+                                    <FontAwesomeIcon
+                                        icon={faArrowUpRightFromSquare}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Fade>
+            <Fade bottom>
+                <div className={styles.swiper_wrap_row}>
+                    <Swiper
+                        slidesPerView={1}
+                        spaceBetween={30}
+                        loop={true}
+                        pagination={{
+                            clickable: true,
+                        }}
+                        navigation={true}
+                        modules={[Pagination, Navigation]}
+                        className={styles.mySwiper_row_black}
+                    >
+                        <SwiperSlide>
+                            <img
+                                src={images.js_1_2_img1}
+                                className={styles.js_product}
+                                alt="작업물 앱 관리자페이지 사진"
+                            />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img
+                                src={images.js_1_2_img2}
+                                className={styles.js_product}
+                                alt="작업물 앱 관리자페이지 사진"
+                            />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img
+                                src={images.js_1_2_img3}
+                                className={styles.js_product}
+                                alt="작업물 앱 관리자페이지 사진"
+                            />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img
+                                src={images.js_1_2_img4}
+                                className={styles.js_product}
+                                alt="작업물 앱 관리자페이지 사진"
+                            />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img
+                                src={images.js_1_2_img5}
+                                className={styles.js_product}
+                                alt="작업물 앱 관리자페이지 사진"
+                            />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img
+                                src={images.js_1_2_img6}
+                                className={styles.js_product}
+                                alt="작업물 앱 관리자페이지 사진"
+                            />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img
+                                src={images.js_1_2_img7}
+                                className={styles.js_product}
+                                alt="작업물 앱 관리자페이지 사진"
+                            />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img
+                                src={images.js_1_2_img8}
+                                className={styles.js_product}
+                                alt="작업물 앱 관리자페이지 사진"
+                            />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img
+                                src={images.js_1_2_img9}
+                                className={styles.js_product}
+                                alt="작업물 앱 관리자페이지 사진"
+                            />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img
+                                src={images.js_1_2_img10}
+                                className={styles.js_product}
+                                alt="작업물 앱 관리자페이지 사진"
+                            />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img
+                                src={images.js_1_2_img11}
+                                className={styles.js_product}
+                                alt="작업물 앱 관리자페이지 사진"
+                            />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img
+                                src={images.js_1_2_img12}
+                                className={styles.js_product}
+                                alt="작업물 앱 관리자페이지 사진"
+                            />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img
+                                src={images.js_1_2_img13}
+                                className={styles.js_product}
+                                alt="작업물 앱 관리자페이지 사진"
+                            />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <img
+                                src={images.js_1_2_img14}
+                                className={styles.js_product}
+                                alt="작업물 앱 관리자페이지 사진"
+                            />
+                        </SwiperSlide>
+                    </Swiper>
+                    <div className={styles.project_content_row}>
+                        <div className={styles.content}>
+                            <div>
+                                위 레시피 공유 및 밀키트 판매 앱의 관리자
+                                페이지입니다.
+                                <br />
+                                PHP와 부트스트랩을 이용해 제작했습니다. 유저와
+                                레시피, 공지, 밀키트 목록, 판매 데이터 시각화
+                                자료 등을 확인할 수 있습니다.
+                            </div>
+                            <div>개발인원: 4명</div>
+                        </div>
+                        <div className={styles.skill_shortcut}>
+                            <div className={styles.skills}>
+                                <div style={{ color: colors.PHP }}>PHP</div>
                                 <div style={{ color: colors.Bootstrap }}>
                                     Bootstrap
                                 </div>
@@ -248,17 +400,21 @@ const JsPractice = () => {
                         </SwiperSlide>
                     </Swiper>
                     <div className={styles.project_content}>
-                        플러터로 제작한 인터벌 타이머 앱입니다.
-                        <br />
-                        우측 하단의 플로팅 버튼을 통해 시간을 설정할 수 있고,
-                        설정된 시간이 계속 반복됩니다.
-                        <br />
-                        좌측 상단의 아이콘을 누르면 기본모드와 다크모드를 설정할
-                        수 있습니다.
-                        <br />
-                        마지막 이미지는 데모버전으로, 웹에서의 모습입니다.
-                        <br />
-                        개발인원: 1명
+                        <div className={styles.content}>
+                            <div>
+                                플러터로 제작한 인터벌 타이머 앱입니다.
+                                <br />
+                                우측 하단의 플로팅 버튼을 통해 시간을 설정할 수
+                                있고, 설정된 시간이 계속 반복됩니다.
+                                <br />
+                                좌측 상단의 아이콘을 누르면 기본모드와
+                                다크모드를 설정할 수 있습니다.
+                                <br />
+                                마지막 이미지는 데모버전으로, 웹에서의
+                                모습입니다.
+                            </div>
+                            <div>개발인원: 1명</div>
+                        </div>
                         <div className={styles.skill_shortcut}>
                             <div className={styles.skills}>
                                 <div style={{ color: colors.Flutter }}>
@@ -328,17 +484,23 @@ const JsPractice = () => {
                         </SwiperSlide>
                     </Swiper>
                     <div className={styles.project_content_row}>
-                        바닐라 자바스크립트로 제작한 투두리스트입니다. <br />
-                        현재 시각을 한 눈에 볼 수 있으며 투두리스트를 작성하기
-                        전, 이름 혹은 닉네임을 설정할 수 있습니다. 리스트를
-                        작성한 후에 옆의 X버튼을 누르면 해당 리스트가
-                        삭제됩니다. 리스트는 로컬 스토리지에 저장되어 브라우저에
-                        재접속 하거나, 새로고침을 해도 남아 있습니다.
-                        <br />
-                        배경 이미지와 하단의 글귀는 새로고침을 할 때마다
-                        랜덤으로 변경됩니다.
-                        <br />
-                        개발인원: 1명
+                        <div className={styles.content}>
+                            <div>
+                                바닐라 자바스크립트로 제작한 투두리스트입니다.{" "}
+                                <br />
+                                현재 시각을 한 눈에 볼 수 있으며 투두리스트를
+                                작성하기 전, 이름 혹은 닉네임을 설정할 수
+                                있습니다. 리스트를 작성한 후에 옆의 X버튼을
+                                누르면 해당 리스트가 삭제됩니다. 리스트는 로컬
+                                스토리지에 저장되어 브라우저에 재접속 하거나,
+                                새로고침을 해도 남아 있습니다.
+                                <br />
+                                배경 이미지와 하단의 글귀는 새로고침을 할 때마다
+                                랜덤으로 변경됩니다.
+                                <br />
+                            </div>
+                            <div>개발인원: 1명</div>
+                        </div>
                         <div className={styles.skill_shortcut}>
                             <div className={styles.skills}>
                                 <div style={{ color: colors.JS }}>JS</div>
@@ -388,9 +550,10 @@ const JsPractice = () => {
                         <SwiperSlide>Slide 3</SwiperSlide>
                     </Swiper>
                     <div className={styles.project_content}>
-                        포트폴리오입니다.
-                        <br />
-                        content
+                        <div className={styles.content}>
+                            <div>포트폴리오입니다.</div>
+                            <div>content</div>
+                        </div>
                         <div className={styles.skill_shortcut}>
                             <div className={styles.skills}>
                                 <div style={{ color: colors.React }}>React</div>
