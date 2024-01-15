ae216a1e48f9cd25e059f4d3947b6ebfb566e3bc
wnal4634 <wnal4634@naver.com>
2024-01-15 20:53:20 +0900

    작업물 슬라이드로 변경, 경험 추가

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 import styles from "css/Experience.module.css";
 import Modal from "react-modal";
-import img1 from "img/공모전_상장.png";
+import img1 from "img/ex/공모전_상장.png";
+import img2 from "img/ex/ankkoDetailPage.png";
+import img3 from "img/ex/ankkoMenu.png";
 import Fade from "react-reveal/Fade";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import {
+    faXmark,
+    faArrowUpRightFromSquare,
+} from "@fortawesome/free-solid-svg-icons";
 import { Mobile, PC } from "components/Responsive";
+import link from "./Link";
 
 const ModalStyle = {
     overlay: {
@@ -63,10 +69,10 @@ const Experience = () => {
 
     return (
         <>
-            <PC>
+            {/* <PC>
                 <Fade bottom>
                     <div id="4" className={styles.experience_wrap}>
-                        <h2>Awards</h2>
+                        <h2>Experience</h2>
                         <hr />
                         <div className={styles.modal_wrap}>
                             <img
@@ -98,8 +104,107 @@ const Experience = () => {
                         </div>
                     </div>
                 </Fade>
+            </PC> */}
+            <PC>
+                <div id="4" className={styles.ex_wrap}>
+                    <h2>Experience</h2>
+                    <hr />
+                    <div className={styles.ex}>
+                        <div className={styles.main_ex}>
+                            <img src={img1} alt="공모전 상장 이미지" />
+                            <div className={styles.ex_text_wrap}>
+                                <div className={styles.box} />
+                                <div className={styles.ex_text}>
+                                    한국정보통신보안윤리학회 주관 공모전 제6회
+                                    산업혁명 인재양성 공유·협업페스티발에
+                                    졸업작품 '레시피 공유 및 밀키트 판매 앱'으로
+                                    출전해 캡스톤경진실적물/기타 부문에서
+                                    우수상을 수상했습니다.
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.sub_ex_wrap}>
+                            <div className={styles.sub_title}>
+                                Lorem ipsum is placeh
+                            </div>
+                            <div className={styles.sub_ex}>
+                                <div className={styles.sub_ex_1}>
+                                    <img src={img3} alt="작업물 이미지" />
+                                    <div className={styles.ex_text_wrap}>
+                                        <div className={styles.box} />
+                                        <div className={styles.ex_text}>
+                                            식품 브랜드 '앙꼬'와 작업한
+                                            메뉴판입니다. 각 메뉴의 일러스트는
+                                            실제 이미지와 근접하도록 직접
+                                            그렸으며, 각 제품에 간단한 설명을
+                                            더해 쉬운 파악이 가능하도록
+                                            제작했습니다.
+                                        </div>
+                                        <div className={styles.ex_url_wrap}>
+                                            Lorem ipsum dolor sit amet,
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className={styles.sub_ex_2}>
+                                    <img src={img2} alt="작업물 이미지" />
+                                    <div className={styles.ex_text_wrap}>
+                                        <div className={styles.box} />
+                                        <div className={styles.ex_text}>
+                                            식품 브랜드 '앙꼬'와의 두 번째
+                                            작업으로, 판매 제품인 도라야끼의
+                                            상세 페이지와 제품 기한카드를
+                                            제작했습니다. 상세 페이지에는 제품의
+                                            종류 및 맛 설명, 다른 브랜드와의
+                                            차별화 등을 담았습니다.
+                                            <br />
+                                            기한카드는 상반기와 하반기 제품으로
+                                            나뉘어 있으며, 판매 제품들의
+                                            소비기한과 보관 방법을 작성했니다.
+                                        </div>
+                                        <div className={styles.ex_url_wrap}>
+                                            <div
+                                                className={styles.ex_url}
+                                                onClick={() => {
+                                                    window.open(
+                                                        link.outsource_use,
+                                                    );
+                                                }}
+                                            >
+                                                <div>
+                                                    사용처
+                                                    <FontAwesomeIcon
+                                                        icon={
+                                                            faArrowUpRightFromSquare
+                                                        }
+                                                    />
+                                                </div>
+                                            </div>
+                                            <div
+                                                className={styles.ex_url}
+                                                onClick={() => {
+                                                    window.open(
+                                                        link.outsource_figma,
+                                                    );
+                                                }}
+                                            >
+                                                <div>
+                                                    피그마
+                                                    <FontAwesomeIcon
+                                                        icon={
+                                                            faArrowUpRightFromSquare
+                                                        }
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </PC>
-            <Mobile>
+            {/* <Mobile>
                 <Fade bottom>
                     <div id="4" className={styles.experience_wrap}>
                         <h2>Awards</h2>
@@ -134,7 +239,7 @@ const Experience = () => {
                         </div>
                     </div>
                 </Fade>
-            </Mobile>
+            </Mobile> */}
         </>
     );
 };
--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,14 +1,83 @@
 import React, { useState } from "react";
-import "css/Slide.css";
+import "css/Slide.scss";
+import link from "./Link";
 
 const slides = [
-    { color: "red", target: "#" },
-    { color: "orange", target: "#" },
-    { color: "yellow", target: "#" },
-    { color: "green", target: "#" },
-    { color: "blue", target: "#" },
-    { color: "navy", target: "#" },
-    { color: "purple", target: "#" },
+    { name: "red", target: "#" },
+    { name: "orange", target: "#" },
+    { name: "yellow", target: "#" },
+    { name: "green", target: "#" },
+    { name: "blue", target: "#" },
+    { name: "navy", target: "#" },
+    { name: "purple", target: "#" },
+];
+const slides2 = [
+    {
+        name: "레시피 공유 및 밀키트 판매 앱",
+        target: "#",
+        img: "",
+        language: "JAVA",
+        language2: "PHP",
+        language3: "MySQL",
+        url: link.recipe_github,
+        url2: link.recipe_figma,
+        team: "팀 프로젝트",
+    },
+    {
+        name: "레시피 공유 및 밀키트 판매 앱의 관리자 페이지",
+        target: "#",
+        img: "",
+        language: "PHP",
+        language2: "Bootstrap",
+        language3: "Python",
+        url: link.recipe_php_github,
+        url2: link.colab,
+        team: "팀 프로젝트",
+    },
+    {
+        name: "인터벌 타이머 앱 ",
+        target: "#",
+        img: "",
+        language: "Flutter",
+        language2: "Dart",
+        language3: "",
+        url: link.interval_github,
+        url2: link.interval_demo,
+        team: "개인 프로젝트",
+    },
+    {
+        name: "투두리스트",
+        target: "#",
+        img: "",
+        language: "JS",
+        language2: "CSS",
+        language3: "",
+        url: link.toDo_github,
+        url2: link.toDo_demo,
+        team: "개인 프로젝트",
+    },
+    {
+        name: "개인 웹 포트폴리오",
+        target: "#",
+        img: "",
+        language: "React",
+        language2: "JS",
+        language3: "CSS",
+        url: link.pf_github,
+        url2: "",
+        team: "개인 프로젝트",
+    },
+    {
+        name: "서울시 실시간 날씨와 대기오염정보를 확인할 수 있는 웹페이지",
+        target: "#",
+        img: "",
+        language: "React",
+        language2: "JS",
+        language3: "CSS",
+        url: link.weatherRec_github,
+        url2: link.weatherRec_demo,
+        team: "개인 프로젝트",
+    },
 ];
 
 const Slide = () => {
@@ -29,30 +98,66 @@ const Slide = () => {
                             animate ? "" : " stop",
                         )}
                     >
-                        {slides.map((s, i) => (
-                            <li
-                                key={i}
-                                className={i % 2 === 0 ? "big" : "small"}
-                            >
+                        {/* {slides.map((s, i) => (
+                            <li key={i} className={"size"}>
                                 <div
                                     className="item"
-                                    style={{ background: s.color }}
+                                    style={{ background: s.name }}
                                 ></div>
+                            </li>
+                        ))} */}
+                        {slides2.map((s, i) => (
+                            <li key={i} className={"size"}>
+                                <div className="item">
+                                    <img src={s.img} alt="작업물 이미지" />
+                                    <div>
+                                        {s.name}
+                                        {s.language}
+                                        {s.language2}
+                                        {s.language3}
+                                    </div>
+                                    <div>
+                                        <a href={s.url} target="_blank">
+                                            깃허브
+                                        </a>
+                                        <a href={s.url2} target="_blank">
+                                            데모
+                                        </a>
+                                    </div>
+                                </div>
                             </li>
                         ))}
                     </div>
                     <div
                         className={"slide clone".concat(animate ? "" : " stop")}
                     >
-                        {slides.map((s, i) => (
-                            <li
-                                key={i}
-                                className={i % 2 === 0 ? "big" : "small"}
-                            >
+                        {/* {slides.map((s, i) => (
+                            <li key={i} className={"size"}>
                                 <div
                                     className="item"
-                                    style={{ background: s.color }}
+                                    style={{ background: s.name }}
                                 ></div>
+                            </li>
+                        ))} */}
+                        {slides2.map((s, i) => (
+                            <li key={i} className={"size"}>
+                                <div className="item">
+                                    <img src={s.img} alt="작업물 이미지" />
+                                    <div>
+                                        {s.name}
+                                        {s.language}
+                                        {s.language2}
+                                        {s.language3}
+                                    </div>
+                                    <div>
+                                        <a href={s.url} target="_blank">
+                                            깃허브
+                                        </a>
+                                        <a href={s.url2} target="_blank">
+                                            데모
+                                        </a>
+                                    </div>
+                                </div>
                             </li>
                         ))}
                     </div>
