1bd0dccde060071df98cca5aa40e8d39bf987553
wnal4634 <wnal4634@naver.com>
2024-01-27 22:05:41 +0900

    스크롤 빼고 프로그레스바

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import "css/App.css";
 
 function App() {
+    const [scroll, setScroll] = useState(0);
+
+    useEffect(() => {
+        let progressBarHandler = () => {
+            const totalScroll = document.documentElement.scrollTop;
+            const windowHeight =
+                document.documentElement.scrollHeight -
+                document.documentElement.clientHeight;
+            const scroll = `${totalScroll / windowHeight}`;
+
+            setScroll(scroll);
+        };
+
+        window.addEventListener("scroll", progressBarHandler);
+
+        return () => window.removeEventListener("scroll", progressBarHandler);
+    });
+
     return (
         <>
+            <div id="progressBarContainer">
+                <div
+                    id="progressBar"
+                    style={{
+                        transform: `scale(1, ${scroll})`,
+                        opacity: 1,
+                        // opacity: `${scroll}`,
+                    }}
+                />
+            </div>
+
             <AppRouter />
         </>
     );
